4c0691fed684f2a061d3daecc80a19dc87e268c0
曾庆国 <zengqingguo@goyoo.com>
2016-09-06 18:22:11 +0800

    用户接口更改

--- core/user/UserInterface.go
+++ core/user/UserInterface.go
@@ -5,4 +5,5 @@ import "alertCenter/models"
 type UserInterface interface {
 	SearchTeams() ([]*models.Team, error)
 	SearchUsers() ([]*models.User, error)
+	GetUserByTeam(id string) ([]*models.User, error)
 }
